1a7ca392958ee454efa0a0edfffe8436be7ae478
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-01-11 16:13:08 +0000

    added the 2 ways to create a card

--- index.js
+++ index.js
@@ -31,10 +31,30 @@ const Card = connection.define(
 const main = async () => {
   try {
     await connection.authenticate();
+    await Card.sync({ alter: true });
+
+    //Create then save in 2 steps
+    // const stuffy_doll = Card.build({
+    //   name: "Stuffy Doll",
+    //   cost: 5,
+    //   description: "Indestructible",
+    // });
+
+    // await stuffy_doll.save();
+
+    // Create then save in 1 step
+    await Card.create({
+      name: "Meteor Golumn",
+      cost: 7,
+      description: "Destroys Target",
+    });
+
     console.log("Connection has successfully established");
   } catch (error) {
     console.error("Unable to connect ", error);
   }
+  await connection.close();
+  process.exit();
 };
 
 main();
