70ca9be0bed79139706387c7962da50509e77665
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-01-11 15:43:35 +0000

    added the Card model

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("index has executed");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const connection = new Sequelize("master32", "root", "Password01", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+const Card = connection.define(
+  "Card",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    cost: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    indexed: [{ unique: true, fields: ["name"] }],
+  }
+);
+
+const main = async () => {
+  try {
+    await connection.authenticate();
+    console.log("Connection has successfully established");
+  } catch (error) {
+    console.error("Unable to connect ", error);
+  }
+};
+
+main();
