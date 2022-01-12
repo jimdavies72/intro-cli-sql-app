cd06a2de96a84ac4c234d558352cc4ba29344bd3
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-01-12 10:45:22 +0000

    added .env and dotenv. updated the connection string

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
 const { Sequelize, DataTypes, Op } = require("sequelize");
 
-const connection = new Sequelize("master32", "root", "Password01", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// const connection = new Sequelize(
+//   process.env.DB_DATABASE,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//   }
+// );
+
+const connection = new Sequelize(process.env.DB_URI);
 
 const Card = connection.define(
   "Card",
